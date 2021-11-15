eb9cdec0ef6d5d8766bf309821bfd91d1e47ee11
Maxxxz1 <msjyuioll@gmail.com>
2021-11-15 15:37:20 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    sum(25,45);
+    out.println(sqrt(250));
+
+    }
+    static void sum(int ...a){
+
+
+    }
+
+}
